12c8a4994b74970c06f3887708bdeb11b0d272c1
AgohChris <agihchris90@gmail.com>
2025-04-18 22:59:12 +0000

    Auto-commit du : 18-04-2025 à 22:59:12

--- CampusConnectback/CampusConnectback/settings.py
+++ CampusConnectback/CampusConnectback/settings.py
@@ -74,12 +74,13 @@ WSGI_APPLICATION = 'CampusConnectback.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    
-
-
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME': 'CampusConnect',
+        'USER' :'chris',
+        'PASSWORD': '1234',
+        'HOST':'127.0.0.1',
+        'PORT':'5432'
     }
 }
 
